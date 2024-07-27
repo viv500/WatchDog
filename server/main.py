de57176c8f50f83305b664bf4b3d4110823b5d0a
Harishan <harishan.g05@gmail.com>
2024-07-27 19:06:42 -0400

    hello

--- server/main.py
+++ server/main.py
@@ -1,2 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+@app.route('/', methods=['GET', 'POST'])
+def home(): 
+    return jsonify(
+        {
+            "users": ['vivek', 'harishan', 'manit']
+        }
+    )
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
