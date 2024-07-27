70233a34eecc090a958a861ea8ac95a9e9e9a276
Harishan <harishan.g05@gmail.com>
2024-07-27 17:17:04 -0400

    React Transcribing Component
    Created a react component that transcribes live audio to text

---
+++ transcript-example/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LiveTranscription from './LiveTranscription';
+
+function App() {
+  return (
+    <>
+      <LiveTranscription/>
+    </>
+  );
+}
+
+export default App;
