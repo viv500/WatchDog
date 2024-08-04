5d18c625a0a33a22adb2ac853c9ab5853f150728
viv500 <vivek.j.manoj@gmail.com>
2024-08-04 00:54:02 -0400

    reconfigured calculation.py

--- server/calculation.py
+++ server/calculation.py
@@ -1,48 +1,88 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 from openai import OpenAI
-from variables import scam_call_sentences
-import json
+from dotenv import load_dotenv
+import os
 
-decrement_value_for_legit_phrases = 3.3
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
-# Load environment variables from .env file
+from calculation import context_matching
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
+
+app = Flask(__name__)
+CORS(app, origins = "http://localhost:5173")
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
 
 client = OpenAI(
     api_key = config["OPENAI_API_KEY"]
 )
 
-def context_matching(sentence, current_score, number_of_scams):
-    prompt1 = '''This is a dictionary with Scam-Phrase: Scam score key value pairs. '''
-    prompt2 = "This is a sentence: "
-    prompt3 = '''You need to context match the sentence to see if it resembles any of the Scam-Phrase keys in
-    the dictionary. if you could match the sentence to a scam-phrase, return a JSON string '{"match": true, "sentence": "the sentence", "score": scam-score for that scam}'
-    if you werent able to find any similarities between sentence and dictionary keys, return JSON string '{"match": false, "sentence": "the sentence", "score": 0}'
-    make sure the sentence in the JSON is in string form'''
+@app.route('/myip', methods=['GET'])
+def my_ip(): 
+    return request.headers.getlist("X-Forwarded-For")[0]
 
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {"role": "user", "content": prompt1 + str(scam_call_sentences) + prompt2 + sentence + prompt3}
-        ],
-        model="gpt-3.5-turbo"
+@app.route('/', methods=['GET'])
+def home(): 
+    return jsonify(
+        {
+            "creators": ['vivek', 'harishan', 'sam', 'prasun']
+        }
     )
 
-    scam_result_json = chat_completion.choices[0].message.content.strip()
+@app.route('/', methods=['POST'])
+def receive_data(): 
+    data = request.json
+    sentence = data['sentence']
+    score = data['score']
+    num_scams = data['num_scams']
+    context_matched = context_matching(sentence, score, num_scams)
+    response = {'isScam': context_matched[0], 
+                    'sentence': context_matched[1], 
+                    'newScore': context_matched[2]
+                } 
+    return jsonify(response)
 
-    try:
-        scam_result = json.loads(scam_result_json)
-    except json.JSONDecodeError:
-        print("Error decoding JSON: ", scam_result_json)
-        return [False, sentence, current_score]
+'''
+json {
+    transcript: full transcript
+}
 
-    if scam_result.get("match"):
-        current_score = (((current_score * number_of_scams) + scam_result["score"]) / (number_of_scams + 1)) + ((number_of_scams)/2)
-    else:
-        current_score -= decrement_value_for_legit_phrases
+send back: {
+    score: 
+    transcript with strong tags around 'scammy' sentences 
+}
+'''
+@app.route('/message', methods=['POST'])
+def parse_transcribe(): 
+    data = request.json
+    transcript = data['transcript'] 
+    parsed = sent_tokenize(transcript)
+    score=0
+    num_scams = 0
+    scanned_transcript = []
+    for sentence in parsed: 
+        context = context_matching(sentence, score, num_scams)
+        score = context[2]
+        if (context[0]): 
+            num_scams += 1
+            sentence = '<strong>' + sentence + '</strong>'
+        scanned_transcript.append(sentence) 
+        
+    scanned_transcript = ' '.join(scanned_transcript)
+    response = {
+        'transcript': scanned_transcript, 
+        'score': score
+    }
+    return jsonify(response)
 
-    return [scam_result.get("match"), sentence, current_score]
-
-# print(context_matching("Can you give me the last 4 digits of your social security number", 45, 2))
-# print(context_matching("We can give you a free trial ummm, uh you only need to pay us a small processing fee", 45, 2))
-# print(context_matching("How has your day been so far Mr. Smith?", 45, 2))
+if __name__ == '__main__': 
+    app.run(debug=True)
